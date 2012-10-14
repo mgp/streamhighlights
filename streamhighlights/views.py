2e3b4a631ffb4dd753eecb1dccd2790bd26d8dc8
Michael Parker <michael.g.parker@gmail.com>
2012-10-14 16:58:33 -0700

    Checkpoint on tests for AJAX requests

--- streamhighlights/views.py
+++ streamhighlights/views.py
@@ -1,6 +1,7 @@
 from streamhighlights import app
 
 import db
+import flask
 import functools
 
 _AJAX_SUCCESS = {'success': True}
@@ -11,52 +12,52 @@ def ajax_request(f):
 	def decorated_function(*pargs, **kwargs):
 		try:
 			# Raise an exception if the session is missing the client identifier.
-			g.client_id = session['client_id']
+			flask.g.client_id = flask.session['client_id']
 			f(*pargs, **kwargs)
-			return jsonify(_AJAX_SUCCESS)
-		except Exception:
-			return jsonify(_AJAX_FAILURE)
+			return flask.jsonify(_AJAX_SUCCESS)
+		except Exception as e:
+			return flask.jsonify(_AJAX_FAILURE)
 	return decorated_function
 
 @app.route('/add_playlist_bookmark', methods=['POST'])
 @ajax_request
 def add_playlist_bookmark():
 	db.add_playlist_bookmark(
-			g.client_id, request.form['playlist_id'], request.form['bookmark_id'])
+			flask.g.client_id, flask.request.form['playlist_id'], flask.request.form['bookmark_id'])
 
 @app.route('/remove_playlist_bookmark', methods=['POST'])
 @ajax_request
 def remove_playlist_bookmark():
 	db.remove_playlist_bookmark(
-				g.client_id, request.form['playlist_id'], request.form['bookmark_id'])
+				flask.g.client_id, flask.request.form['playlist_id'], flask.request.form['bookmark_id'])
 
 @app.route('/vote_playlist_thumb_up', methods=['POST'])
 @ajax_request
 def vote_playlist_thumb_up():
-	db.vote_playlist_thumb_up(g.client_id, request.form['playlist_id'])
+	db.vote_playlist_thumb_up(flask.g.client_id, flask.request.form['playlist_id'])
 
 @app.route('/vote_playlist_thumb_down', methods=['POST'])
 @ajax_request
 def vote_playlist_thumb_down():
-	db.vote_playlist_thumb_down(g.client_id, request.form['playlist_id'])
+	db.vote_playlist_thumb_down(flask.g.client_id, flask.request.form['playlist_id'])
 
 @app.route('/remove_playlist_vote', methods=['POST'])
 @ajax_request
 def remove_playlist_vote():
-	db.remove_playlist_vote(g.client_id, request.form['playlist_id'])
+	db.remove_playlist_vote(flask.g.client_id, flask.request.form['playlist_id'])
 
 @app.route('/vote_bookmark_thumb_up', methods=['POST'])
 @ajax_request
 def vote_bookmark_thumb_up():
-	db.vote_bookmark_thumb_up(g.client_id, request.form['bookmark_id'])
+	db.vote_bookmark_thumb_up(flask.g.client_id, flask.request.form['bookmark_id'])
 
 @app.route('/vote_bookmark_thumb_down', methods=['POST'])
 @ajax_request
 def vote_bookmark_thumb_down():
-	db.vote_bookmark_thumb_down(g.client_id, request.form['bookmark_id'])
+	db.vote_bookmark_thumb_down(flask.g.client_id, flask.request.form['bookmark_id'])
 
 @app.route('/remove_bookmark_vote', methods=['POST'])
 @ajax_request
 def remove_bookmark_vote():
-	db.remove_bookmark_vote(g.client_id, request.form['bookmark_id'])
+	db.remove_bookmark_vote(flask.g.client_id, flask.request.form['bookmark_id'])
 
--- streamhighlights/views_tests.py
+++ streamhighlights/views_tests.py
@@ -1,51 +1,139 @@
+from streamhighlights import app
+
+from datetime import datetime
 import json
 import requests
 import unittest
+
+import db
 import views
 
-class TestViews(unittest.TestCase):
+class Foo:
+	"""Utility method for creating a user."""
+	def _create_user(self, name):
+		user = db.User(name, self.now)
+		self.session.add(user)
+		self.session.commit()
+		return user.id
+
+	"""Utility method for creating a video."""
+	def _create_video(self, name, length):
+		video = db.Video(name, length)
+		self.session.add(video)
+		self.session.commit()
+		return video.id
+
+	"""Utility method for creating a bookmark."""
+	def _create_bookmark(self, user_id, video_id, comment, time):
+		bookmark = db.Bookmark(
+				comment, time, self.now, user_id=user_id, video_id=video_id)
+		self.session.add(bookmark)
+		self.session.commit()
+		return bookmark.id
+
+
+class TestViews(unittest.TestCase, Foo):
 	def setUp(self):
-		self.client_id = 'client_id'
+		unittest.TestCase.setUp(self)
+		self.now = datetime(2012, 10, 15, 12, 30, 45)
+		self.session = db.session
+		db.create_all()
 	
 	def tearDown(self):
-		pass
+		unittest.TestCase.tearDown(self)
+		db.drop_all()
 	
-	def _add_client_id(self, client):
-		with client.session_transaction() as session
-			session['client_id'] = self.client_id
+	def _add_client_id(self, client, client_id):
+		with client.session_transaction() as session:
+			session['client_id'] = client_id
 
+	"""Assert that the AJAX request succeeded.
+	"""
 	def _assert_ajax_success(self, response):
 		self.assertEqual(requests.codes.ok, response.status_code)
 		json_response = json.loads(response.data)
 		self.assertTrue(json_response['success'])
 	
+	"""Assert that the AJAX request failed.
+	"""
 	def _assert_ajax_failure(self, response):
 		self.assertEqual(requests.codes.ok, response.status_code)
 		json_response = json.loads(response.data)
 		self.assertFalse(json_response['success'])
 	
 	def test_add_playlist_bookmark(self):
-		playlist_id = 'playlist_id'
-		bookmark_id = 'bookmark_id'
+		client_id = self._create_user('user_name')
+		video_id = self._create_video('video_name', 99)
+		bookmark_id = self._create_bookmark(client_id, video_id, 'comment', 33)
+		playlist_id = db.create_playlist(client_id, 'playlist_name', now=self.now)
 
 		with app.test_client() as client:
-			self._add_client_id(client)
+			self._add_client_id(client, client_id)
 			response = client.post('/add_playlist_bookmark',
 					data={'playlist_id': playlist_id, 'bookmark_id': bookmark_id})
 			self._assert_ajax_success(response)
 	
 	def test_add_playlist_bookmark_bad_request(self):
+		client_id = self._create_user('user_name')
+		video_id = self._create_video('video_name', 99)
+		bookmark_id = self._create_bookmark(client_id, video_id, 'comment', 33)
+		playlist_id = db.create_playlist(client_id, 'playlist_name', now=self.now)
+
 		# Assert that the request fails with a missing client identifier.
-		# TODO
+		with app.test_client() as client:
+			response = client.post('/add_playlist_bookmark',
+					data={'playlist_id': playlist_id, 'bookmark_id': bookmark_id})
+			self._assert_ajax_failure(response)
 
 		# Assert that the request fails with a missing playlist identifier.
-		# TODO
+		with app.test_client() as client:
+			self._add_client_id(client, client_id)
+			response = client.post('/add_playlist_bookmark', data={'bookmark_id': bookmark_id})
+			self._assert_ajax_failure(response)
 	
+		# Assert that the request fails with a missing bookmark identifier.
+		with app.test_client() as client:
+			self._add_client_id(client, client_id)
+			response = client.post('/add_playlist_bookmark', data={'playlist_id': playlist_id})
+			self._assert_ajax_failure(response)
+
 	def test_remove_playlist_bookmark(self):
-		pass
-	
+		client_id = self._create_user('user_name')
+		video_id = self._create_video('video_name', 99)
+		bookmark_id = self._create_bookmark(client_id, video_id, 'comment', 33)
+		playlist_id = db.create_playlist(client_id, 'playlist_name', now=self.now)
+		db.add_playlist_bookmark(client_id, playlist_id, bookmark_id, now=self.now)
+
+		with app.test_client() as client:
+			self._add_client_id(client, client_id)
+			response = client.post('/remove_playlist_bookmark',
+					data={'playlist_id': playlist_id, 'bookmark_id': bookmark_id})
+			self._assert_ajax_success(response)
+		
 	def test_remove_playlist_bookmark_bad_request(self):
-		pass
+		client_id = self._create_user('user_name')
+		video_id = self._create_video('video_name', 99)
+		bookmark_id = self._create_bookmark(client_id, video_id, 'comment', 33)
+		playlist_id = db.create_playlist(client_id, 'playlist_name', now=self.now)
+		db.add_playlist_bookmark(client_id, playlist_id, bookmark_id, now=self.now)
+
+		# Assert that the request fails with a missing client identifier.
+		with app.test_client() as client:
+			response = client.post('/remove_playlist_bookmark',
+					data={'playlist_id': playlist_id, 'bookmark_id': bookmark_id})
+			self._assert_ajax_failure(response)
+
+		# Assert that the request fails with a missing playlist identifier.
+		with app.test_client() as client:
+			self._add_client_id(client, client_id)
+			response = client.post('/remove_playlist_bookmark', data={'bookmark_id': bookmark_id})
+			self._assert_ajax_success(response)
+
+		# Assert that the request fails with a missing bookmark identifier.
+		with app.test_client() as client:
+			self._add_client_id(client, client_id)
+			response = client.post('/remove_playlist_bookmark', data={'playlist_id': playlist_id})
+			self._assert_ajax_failure(response)
 	
 	def test_vote_playlist_thumb_up(self):
 		pass
@@ -81,5 +169,5 @@ class TestViews(unittest.TestCase):
 		pass
 
 	def test_remove_bookmark_vote_bad_request(self):
-
+		pass
 
