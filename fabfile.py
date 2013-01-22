36bafbd37d4a7d6a0106e10ed8c91d80752c29b8
Michael Parker <michael.g.parker@gmail.com>
2013-01-22 00:13:03 -0800

    Update the fabfile

--- fabfile.py
+++ fabfile.py
@@ -4,14 +4,22 @@ env.hosts = ['msg']
 env.port = 941
 
 def prepare_dist():
+	# Create the empty directory.
 	local('rm -rf dist/')
 	local('mkdir dist')
+	# Copy all files into it.
 	local('cp run_msg_server.py dist')
 	local('cp -r matchstreamguide dist')
 	with lcd('dist'):
+		# Remove unnecessary files.
 		local('find . -name "*.pyc" | xargs -n1 rm')
 		local('find . -name ".DS_Store" | xargs -n1 rm')
-		local('rm -rf matchstreamguide/static/gen')
-		local('rm -rf matchstreamguide/static/.webassets-cache')
+		with lcd('matchstreamguide'):
+			local('rm db_test_case.py *_tests.py')
+			with lcd('static'):
+				local('rm select2.js')
+				local('rm -rf gen')
+				local('rm -rf .webassets-cache')
+		# Create the zipped tarball.
 		local('tar czf msg.tar.gz *')
 
